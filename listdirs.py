2e9304be9fee5cf114322cc10bccf3c258a05880
binayaj <39484567+binayaj@users.noreply.github.com>
2020-02-17 14:09:49 -0600

    Update listdirs.py

--- listdirs.py
+++ listdirs.py
@@ -5,5 +5,5 @@ for dir,subdir,file in os.walk (path):
    modified=os.path.getmtime(dir)
    print ("Modified on ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
 #accessed=os.path.getatime(file)
-print ("Modified on ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
+print ("Modified on this ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
 #print ("Accessed on ", time.strftime('%m/%d/%Y', time.gmtime (accessed)))
