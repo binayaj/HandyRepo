667495e9073a8042bcd7bbc06570bf324b13b6ac
Binaya Joshi <bjoshi@everest.oitsec.umn.edu>
2020-02-18 13:17:23 -0600

    Checking Empty Dirs

--- listdirs.py
+++ listdirs.py
@@ -2,13 +2,18 @@
 
 import os,sys
 import time,datetime
+emptydirs=0
 if ( (len (sys.argv)==2) and os.path.isdir(sys.argv[1])):
     filename="/tmp/"+str(time.time()).split(".")[0]+".csv"
     if not os.path.exists (filename):
         with open (filename,'w') as fp:
          for dir,subdir,file in os.walk (sys.argv[1]):
-             modified=os.path.getmtime(dir)
-             fp.write ('{0},{1}\n '.format(dir,time.strftime('%m/%d/%Y', time.gmtime (modified) ) )) 
+             if not (len (subdir)==0 and len (file)==0):
+               modified=os.path.getmtime(dir)
+               fp.write ('{0},{1}\n '.format(dir,time.strftime('%m/%d/%Y', time.gmtime (modified) ) )) 
+             else:
+               fp.write ('{0},{1}\n '.format(dir, "Empty Directory"))
+
          fp.close()
     else:
 
