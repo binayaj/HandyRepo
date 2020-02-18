f4d75efc79dd24227ea8893841dafdc8049ee570
Binaya Joshi <bjoshi@everest.oitsec.umn.edu>
2020-02-18 11:59:41 -0600

    Final v1

--- listdirs.py
+++ listdirs.py
@@ -1,8 +1,18 @@
+# Gets timestamp of all sub directories
+
 import os,sys
 import time,datetime
 if ( (len (sys.argv)==2) and os.path.isdir(sys.argv[1])):
-    for dir,subdir,file in os.walk (sys.argv[1]):
-        modified=os.path.getmtime(dir)
-        print ('{0},{1} '.format(dir,time.strftime('%m/%d/%Y', time.gmtime (modified) ) )) 
+    filename="/tmp/"+str(time.time()).split(".")[0]+".csv"
+    if not os.path.exists (filename):
+        with open (filename,'w') as fp:
+         for dir,subdir,file in os.walk (sys.argv[1]):
+             modified=os.path.getmtime(dir)
+             fp.write ('{0},{1}\n '.format(dir,time.strftime('%m/%d/%Y', time.gmtime (modified) ) )) 
+         fp.close()
+    else:
+
+           print ("Output file already exists")
 else:
    print ("Usage: listdir.py dirname")
+print ("Results saved to {}".format(filename))
