6c980b17ed4856324fa9e1d7b47d593c47f768ca
Binaya Joshi <bjoshi@everest.oitsec.umn.edu>
2020-02-18 10:01:56 -0600

    version 1

--- listdirs.py
+++ listdirs.py
@@ -1,9 +1,6 @@
 import os
 import time,datetime
-path="/var/"
+
 for dir,subdir,file in os.walk (path):
    modified=os.path.getmtime(dir)
    print ("Modified on ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
-#accessed=os.path.getatime(file)
-print ("Modified on this ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
-#print ("Accessed on ", time.strftime('%m/%d/%Y', time.gmtime (accessed)))
