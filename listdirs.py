05e885876d2db349593047961e6241a127c1579a
Binaya Joshi <bjoshi@everest.oitsec.umn.edu>
2020-02-18 10:29:30 -0600

    updated version

--- listdirs.py
+++ listdirs.py
@@ -1,6 +1,8 @@
-import os
+import os,sys
 import time,datetime
-
-for dir,subdir,file in os.walk (path):
-   modified=os.path.getmtime(dir)
-   print ("Modified on ", time.strftime('%m/%d/%Y', time.gmtime (modified)))
+if ( (len (sys.argv)==2) and os.path.isdir(sys.argv[1])):
+    for dir,subdir,file in os.walk (sys.argv[1]):
+        modified=os.path.getmtime(dir)
+        print ('{0},{1} '.format(dir,time.strftime('%m/%d/%Y', time.gmtime (modified) ) )) 
+else:
+   print ("Usage: listdir.py dirname")
